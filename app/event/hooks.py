7ee74671e1838d1d61fd0c718b590d28667f4a2a
Xavier Bustamante Talavera <xavier@bustawin.com>
2015-11-20 09:13:41 +0100

    New implementation of snapshot. Added register with hability to standalone register. Bugfixes. known issue: no unique checking for hid.

--- app/event/hooks.py
+++ app/event/hooks.py
@@ -7,45 +7,6 @@ from app.exceptions import NoPlaceForGivenCoordinates
 from .event import Event
 
 
-def embed(resource, response):
-    """
-    embed events to Snapshot
-    :param resource:
-    :param response:
-    :return:
-    """
-    import json
-    if resource in Event.resource_types() or resource == 'events':
-        try:
-            embedded = json.loads(request.args.get('embedded'))
-            if embedded.get('events', 0) == 1:
-                for event in response['_items']:
-                    if event['@type'] == 'Snapshot':
-                        ##todo hacerse con GET o embed components no funciona bien :-(
-                        event['events'] = list(app.data.driver.db['events'].find(
-                            {'_id': {'$in': [ObjectId(e) for e in event['events']]}}))
-        except TypeError:
-            pass
-
-
-            # def get_events_for_components(resource: str, request, lookup):
-        """
-    When retrieving for the events of a device, appends all the events where the device is a component.
-    :param request:
-    :return:
-    """
-        ##  if resource == 'events':
-        # lookup['$or'] = [{'components': {'$exists': True}}, {'components': {'$exists': False}}]
-        # lookup['$or'] = [{'components': {'$exists': False}},
-        #                 {'components': {'$in': [ObjectId(request.view_args['device'])]}},
-        #                 {'device': ObjectId(request.view_args['device'])}]
-        # lookup.update({'components': {'$in': [ObjectId(request.view_args['device'])]}})
-
-
-        # del request.view_args['device']
-        #   pass
-
-
 def get_place(resource_name: str, events: list):
     if resource_name in Event.resource_types():
         for event in events:
@@ -61,12 +22,14 @@ def get_place(resource_name: str, events: list):
                             }
                         }
                     })
+                    if not place:
+                        raise NoPlaceForGivenCoordinates()
+                except (KeyError, NoPlaceForGivenCoordinates) as e:
+                    if resource_name == 'receive' or resource_name == 'locate':
+                        raise e
+                else:
                     if 'place' in event:
                         if event['place']['_id'] != str(place['_id']):  # geo 1 place 1
                             raise CoordinatesAndPlaceDoNotMatch()
-                        else:
-                            event['place'] = place['_id']  # geo 1 place found in DB
-                except KeyError:
-                    if (resource_name == 'receive' or resource_name == 'locate') and 'place' not in event:
-                        raise NoPlaceForGivenCoordinates()  # geo 1 place not found in DB.
-                        # Place is just strictly needed for Receive and locate.
+                    else:
+                        event['place'] = place['_id']  # geo 1 place found in DB

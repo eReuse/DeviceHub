4425cdc41f4cfca1dd1bafe3a48837e9e94054b1
bustawin <xavier@bustawin.com>
2016-02-20 12:45:22 +0100

    Add deletions to places and bugfix places

--- app/place/hooks.py
+++ app/place/hooks.py
@@ -1,15 +1,12 @@
-from bson import ObjectId
-
-from app.utils import difference
 from app.app import app
-from app.device.device import Device
+from app.place.place import CannotDeleteIfHasEvent, Place
 
 
 def set_place_in_devices(items: list):
     for item in items:
         if 'devices' in item:
             for device_id in item['devices']:
-                _device_set_place(device_id, item['_id'])
+                Place.device_set_place(device_id, item['_id'])
 
 
 def update_place_in_devices_if_places(updated_place: dict, original_place: dict):
@@ -18,36 +15,14 @@ def update_place_in_devices_if_places(updated_place: dict, original_place: dict)
 
 
 def update_place_in_devices(replaced_place: dict, original_place: dict):
-    original = set(original_place['devices'])
-    updated = set(replaced_place['devices'])
-
-    devices_to_remove_id = original - updated
-    children_to_remove_id = Device.get_components_in_set(devices_to_remove_id)
-    devices_to_remove_id |= children_to_remove_id
-
-    devices_to_add_id = updated - original
-    children_to_add_id = Device.get_components_in_set(devices_to_add_id)
-    devices_to_add_id |= children_to_add_id
-    total_devices_in_place = updated - devices_to_remove_id | devices_to_add_id
-
-    for device_id in devices_to_remove_id:
-        _device_unset_place(device_id)
-    for device_id in devices_to_add_id:
-        _device_set_place(device_id, replaced_place['_id'])
-    app.data.driver.db['devices'].update_one({'_id': replaced_place['_id']}, {'$set': {'devices': list(total_devices_in_place)}})
+    Place.update_devices(set(original_place['devices']), set(replaced_place['devices']), replaced_place['_id'])
 
 
-def unset_place_in_devices(item):
-    for device_id in item['devices']:
-        _device_unset_place(device_id)
+def unset_place_in_devices(place):
+    Place.update_devices(set(place['devices']), set(), None)
 
 
-def _device_set_place(device_id: str, place_id: str):
-    device = app.data.driver.db['devices'].find_one({'_id': device_id}, {'place': True})
-    if 'place' in device:
-        app.data.driver.db['places'].update_one({'_id': device['place']}, {'$pull': {'devices': device_id}})
-    app.data.driver.db['devices'].update_one({'_id': device_id}, {'$set': {'place': place_id}})
-
-
-def _device_unset_place(device_id: str):
-    app.data.driver.db['devices'].update_one({'_id': device_id}, {'$unset': {'place': ''}})
+def avoid_deleting_if_has_event(item):
+    event = app.data.driver.db['events'].find_one({'place': item['_id']}, {'_id': True})
+    if event is not None:
+        raise CannotDeleteIfHasEvent()
--- app/place/place.py
+++ app/place/place.py
@@ -1,7 +1,48 @@
 from app.app import app
+from app.device.device import Device
+from app.exceptions import StandardError
 
 
 class Place:
     @staticmethod
     def get(_id, projections):
         return app.data.driver.db['places'].find_one({'_id': _id}, projections)
+
+    @staticmethod
+    def update_devices(original: set, updated: set, replaced_place_id: str or None):
+        """
+        Given the original set of devices of a place, and the updated version, it updates the database to achieve
+        the updated set of devices.
+
+        :param original:
+        :param updated:
+        :param replaced_place_id: The identifier of the replaced place. If updated is empty, this value is not used.
+        :return:
+        """
+        devices_to_remove_id = original - updated
+        children_to_remove_id = Device.get_components_in_set(devices_to_remove_id)
+        devices_to_remove_id |= children_to_remove_id
+
+        devices_to_add_id = updated - original
+        children_to_add_id = Device.get_components_in_set(devices_to_add_id)
+        devices_to_add_id |= children_to_add_id
+
+        for device_id in devices_to_remove_id:
+            Place.device_unset_place(device_id)
+        for device_id in devices_to_add_id:
+            Place.device_set_place(device_id, replaced_place_id)
+
+    @staticmethod
+    def device_set_place(device_id: str, place_id: str):
+        device = app.data.driver.db['devices'].find_one({'_id': device_id}, {'place': True})
+        if 'place' in device:
+            app.data.driver.db['places'].update_one({'_id': device['place']}, {'$pull': {'devices': device_id}})
+        app.data.driver.db['devices'].update_one({'_id': device_id}, {'$set': {'place': place_id}})
+
+    @staticmethod
+    def device_unset_place(device_id: str):
+        app.data.driver.db['devices'].update_one({'_id': device_id}, {'$unset': {'place': ''}})
+
+
+class CannotDeleteIfHasEvent(StandardError):
+    message = "You cannot delete a place where you performed an event."

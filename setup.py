9be3107527ba8146a96446173157ecbc1194a2b9
bustawin <xavier@bustawin.com>
2016-04-04 00:21:22 +0200

    Change name to eReuse-DeviceHub

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DeviceHub',
+    name='eReuse-DeviceHub',
     version='0.1',
     packages=find_packages(),
     url='https://github.com/eReuse/DeviceHub',

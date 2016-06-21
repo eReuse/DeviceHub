f5f175df0959972c52c669c9525c910325c40632
bustawin <xavier@bustawin.com>
2016-06-21 20:27:49 +0200

    Add scripts to generate a dummy database with devices and easy creating an account through terminal

---
+++ ereuse_devicehub/scripts/dummy_db.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+from ereuse_devicehub.tests.test_resources.test_events.test_snapshot.test_snapshot import TestSnapshot
+
+
+class DummyDB:
+    """
+        Simple class to create a database from 0, adding a dummy user and some devices. Ready to test!
+
+        To use it do the following in your app.py::
+
+            app = DeviceHub()
+            d = DummyDB(app)
+            d.create_dummy_devices()
+
+        This method uses the test class of TestSnapshot, and the credentials in
+        :func:`ereuse_devicehub.tests.TestBase.create_dummy_user`.
+
+    """
+    def __init__(self, app):
+        self.app = app
+        self.test_snapshot = TestSnapshot()
+        self.test_snapshot.app = self.app
+        self.test_snapshot.prepare()
+        self.test_snapshot.create_dummy_user()
+
+    def create_dummy_devices(self):
+        self.test_snapshot.creation = self._creation
+        self.test_snapshot.test_snapshot_2015_12_09()
+        pprint('Finished basic creation of devices.')
+
+    def _creation(self, input_snapshot, *args, **kwargs):
+        self.test_snapshot.post_and_check('events/snapshot', input_snapshot)

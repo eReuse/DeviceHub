02ecab45ee74d959db0ec779a401f54cdeabf385
Xavier Bustamante Talavera <xavier@bustawin.com>
2016-10-09 16:42:42 +0200

    unregisteredTo uses only the requested database for the user
    (cherry picked from commit 2b469a6c448e4fadcdafc3c48332a29adc411b13)

--- ereuse_devicehub/resources/account/hooks.py
+++ ereuse_devicehub/resources/account/hooks.py
@@ -75,7 +75,7 @@ def _add_or_get_inactive_account_id(event, field_name, recipient_field_name):
                                         }
                                         )['_id']
         except TypeError:  # No account
-            event[field_name]['databases'] = AccountDomain.actual['databases']
+            event[field_name]['databases'] = [AccountDomain.get_requested_database()]
             event[field_name]['active'] = False
             event[field_name]['@type'] = 'Account'
             _id = execute_post('accounts', event[field_name])['_id']

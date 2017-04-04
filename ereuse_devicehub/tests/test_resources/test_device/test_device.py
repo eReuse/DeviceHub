750f312da14239b9cd9ff1c5d6da9aa647bae3b9
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-04-04 12:36:50 +0200

    Fix 'test_materializations_events'
    (cherry picked from commit 78d5eee)

--- ereuse_devicehub/tests/test_resources/test_device/test_device.py
+++ ereuse_devicehub/tests/test_resources/test_device/test_device.py
@@ -1,9 +1,10 @@
 from assertpy import assert_that
 
 from ereuse_devicehub.resources.device.component.hard_drive.settings import HardDrive
-from ereuse_devicehub.resources.event.device import DeviceEventDomain
 from ereuse_devicehub.resources.event.device.add.settings import Add
+from ereuse_devicehub.resources.event.device.register.settings import Register
 from ereuse_devicehub.resources.event.device.remove.settings import Remove
+from ereuse_devicehub.resources.event.device.snapshot.settings import Snapshot
 from ereuse_devicehub.tests import TestStandard
 
 
@@ -21,20 +22,17 @@ class TestDevice(TestStandard):
         devices = self.get_fixtures_computers()
         vaio, _ = self.get(self.DEVICES, '', devices[0])
         account_id = str(self.account['_id'])
-        materialized = [
-            {'@type': DeviceEventDomain.new_type('Snapshot'), 'secured': False, 'byUser': account_id,
-             'incidence': False},
-            {'@type': DeviceEventDomain.new_type('Register'), 'secured': False, 'byUser': account_id,
-             'incidence': False},
+        materialized_events = [
+            {'@type': Snapshot.type_name, 'secured': False, 'byUser': account_id, 'incidence': False},
+            {'@type': Register.type_name, 'secured': False, 'byUser': account_id, 'incidence': False},
         ]
         fields = {'@type', '_id', 'byUser', 'incidence', 'secured', '_updated'}
+        fields_snapshot = fields | {'snapshotSoftware'}
         self.assertIn('events', vaio)
-        i = 0
-        for event in vaio['events']:
-            # Order needs to be preserved
-            assert_that(materialized[i]).is_subset_of(event)
-            assert_that(set(event.keys())).is_equal_to(fields)
-            i += 1
+        for event, materialized_event in zip(vaio['events'], materialized_events):
+            assert_that(materialized_event).is_subset_of(event)  # Order needs to be preserved
+            fields_to_check = fields if materialized_event['@type'] != 'devices:Snapshot' else fields_snapshot
+            assert_that(set(event.keys())).is_equal_to(fields_to_check)
 
     def test_computer_materialization_fields(self):
         devices = self.get_fixtures_computers()

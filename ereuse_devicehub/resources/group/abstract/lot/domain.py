7a2c7a388d45b9e6f5fea98196e17077f83a446d
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-06-19 18:37:03 +0200

    Fix operating with pallets and lots

--- ereuse_devicehub/resources/group/abstract/lot/domain.py
+++ ereuse_devicehub/resources/group/abstract/lot/domain.py
@@ -5,6 +5,7 @@ from ereuse_devicehub.resources.domain import Domain
 from ereuse_devicehub.resources.group.abstract.domain import AbstractDomain
 from ereuse_devicehub.resources.group.abstract.lot.settings import Lot, LotSettings
 from ereuse_devicehub.resources.group.physical.package.settings import Package
+from ereuse_devicehub.resources.group.physical.pallet.settings import Pallet
 
 
 class LotDomain(AbstractDomain):
@@ -17,7 +18,7 @@ class LotDomain(AbstractDomain):
         groups_inherit_lot = (Lot.resource_name,)
         if child_resource_name in Lot.resource_names:
             super().inherit(parent_id, parent_ancestors, child_domain, children)
-        elif child_resource_name in Package.resource_names:
+        elif child_resource_name in Package.resource_names | Pallet.resource_names:
             full_children = cls._inherit(groups_inherit_lot, parent_id, parent_ancestors, child_domain, children)
             cls._update_inheritance_grandchildren(full_children, child_domain)
         elif child_resource_name in Device.resource_names:

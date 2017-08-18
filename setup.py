d98a157195db9c4fc6412d830df82a9e0217dad1
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-08-18 11:44:48 +0200

    Force flask to 0.11

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'inflection>=0.3.1',
         'eve>=0.6.3,<0.8',
-        'flask>=0.11',
+        'flask>=0.11,<0.12',  # eve < 0.8 breaks with flask 0.12
         'passlib>=1.6.5,<2.0',
         'validators>=0.10,<0.20',
         'requests>=2.9.1,<3.0',

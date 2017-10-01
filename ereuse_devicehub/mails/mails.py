9778428a37af6b070f91de971828c621ae468810
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-10-01 22:31:30 +0200

    Add subject to mails

--- ereuse_devicehub/mails/mails.py
+++ ereuse_devicehub/mails/mails.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_mail import Message
+from flask_mail import Message, email_dispatched
 from pydash import defaults
 
 """
@@ -17,17 +17,26 @@ TITLES = {
 }
 
 
-def render_mail_template(template_name: str, recipient: dict, **context) -> str:
+def render_mail_template(template_name: str, recipient: dict, **context) -> (str, str):
     """Adds default template variables and renders the template."""
     context = defaults(context, {
         'recipient': recipient,
         'title': TITLES[template_name]
 
     })
-    return render_template(template_name, **context)
+    return render_template(template_name, **context), TITLES[template_name]
 
 
 def create_email(template_name: str, recipient: dict, **context) -> Message:
     """Creates a mail with html."""
-    html = render_mail_template(template_name, recipient, **context)
-    return Message(html=html, recipients=[recipient['email']])
+    html, title = render_mail_template(template_name, recipient, **context)
+    return Message(html=html, recipients=[recipient['email']], subject=title)
+
+
+# Log messages
+# From http://pythonhosted.org/Flask-Mail/#signalling-support
+def log_message(message: Message, app):
+    app.logger.info('Sent message {} to {}.'.format(message.subject, message.recipients))
+
+
+email_dispatched.connect(log_message)
--- ereuse_devicehub/resources/event/device/sell/hooks.py
+++ ereuse_devicehub/resources/event/device/sell/hooks.py
@@ -1,9 +1,8 @@
 from typing import List
 
 from flask import current_app as app
-from flask_mail import Message
 
-from ereuse_devicehub.mails.mails import render_mail_template
+from ereuse_devicehub.mails.mails import create_email
 from ereuse_devicehub.resources.account.domain import AccountDomain
 from ereuse_devicehub.resources.device.domain import DeviceDomain
 from ereuse_devicehub.resources.event.device.reserve.settings import Reserve
@@ -23,6 +22,5 @@ def notify(sells: List[dict]):
             'sell_url': url_for_resource(Reserve.resource_name, sell['_id']),
         }
         to = AccountDomain.get_one(sell['to'])
-        html = render_mail_template('mails/sell.html', recipient=to, **context)
-        msg = Message(html=html, recipients=[to['email']])
+        msg = create_email('mails/sell.html', to, **context)
         app.mail.send(msg)

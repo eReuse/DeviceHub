59715f0e8260dcaaea708399979132adcb8b764d
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-10-13 17:34:35 +0200

    Add db parameter in share_group and enhance the code

--- ereuse_devicehub/scripts/share_group.py
+++ ereuse_devicehub/scripts/share_group.py
@@ -1,16 +1,16 @@
 import argparse
 import json
 
-from pydash import find
-
 from ereuse_devicehub import DeviceHub
+from ereuse_devicehub.resources.group.settings import Group
 from ereuse_devicehub.security.perms import READ
 from ereuse_devicehub.tests import Client
 from ereuse_devicehub.utils import Naming
+from pydash import find
 
 
 def share_group(app: DeviceHub, email: str, password: str, group_type: str, receiver_email: str, perm=READ,
-                group_id: str = None, group_name=None):
+                group_id: str = None, group_label=None, db: str = None):
     """
     Sets the permission for the user in the group, known as sharing. If the user had an existing permission,
     this is replaced.
@@ -24,16 +24,23 @@ def share_group(app: DeviceHub, email: str, password: str, group_type: str, rece
     :param receiver_email: The e-mail of the user to share the group to.
     :param perm:
     :param group_id: Optional. The identifier of the group. Name xor group id needed.
-    :param group_name: Optional. The name of the group. Name xor group id needed.
+    :param group_label: Optional. The name of the group. Name xor group id needed.
+    :param db: The database where the group is in.
     """
-    if not group_id and not group_name:
+    if not group_id and not group_label:
         raise ValueError('Group name xor group_id needed.')
+    if group_type not in Group.types:
+        raise ValueError('Group type should be one of {}'.format(Group.types))
     c = Client(app=app)
     c.prepare()
     token = c.login(email, password)['token']
-    kwargs = {'params': {'where': json.dumps({'label': group_name})}} if group_name else {'item': group_id}
-    kwargs['token'] = token
-    group = c.get_200(Naming.resource(group_type), **kwargs)['_items'][0]
+    kwargs = {'token': token, 'db': db}
+    if group_label:
+        kwargs['params'] = {'where': json.dumps({'label': group_label})}
+    else:
+        kwargs['item'] = group_id
+    response = c.get_200(Naming.resource(group_type), **kwargs)
+    group = response['_items'][0] if group_label else response
     account_params = {'where': json.dumps({'email': receiver_email})}
     receiver = c.get_200(c.ACCOUNTS, params=account_params, token=token)['_items'][0]
     existing_perm = find(group.setdefault('perms', []), {'_id': receiver['_id']})
@@ -42,7 +49,7 @@ def share_group(app: DeviceHub, email: str, password: str, group_type: str, rece
     else:
         group['perms'].append({'account': receiver['_id'], 'perm': perm})
     group_patch = {'@type': group_type, 'perms': group['perms']}
-    return c.patch_200(Naming.resource(group_type), item=group['_id'], data=group_patch, token=token)
+    return c.patch_200(Naming.resource(group_type), item=group['_id'], data=group_patch, token=token, db=db)
 
 
 def main(app):
@@ -63,10 +70,11 @@ def main(app):
     parser.add_argument('group_type')
     parser.add_argument('receiver_email', help='The email of the user that this is being shared to.')
     parser.add_argument('-i', '--group_id', help='The group id.')
-    parser.add_argument('-n', '--group_name', help='The group name. If set, this is used over the id. '
+    parser.add_argument('-l', '--group_label', help='The group name. If set, this is used over the id. '
                                                    'Note that names are not unique, use the id when possible.')
     # We need to set default or this will be ``None``
     parser.add_argument('-p', '--perm', help='The permission the user will have. READ by default.', default=READ)
+    parser.add_argument('-d', '--db', help='The database of the group. Otherwise the default database of the user.')
     args = vars(parser.parse_args())
     response = share_group(app, **args)
     print('Response:')

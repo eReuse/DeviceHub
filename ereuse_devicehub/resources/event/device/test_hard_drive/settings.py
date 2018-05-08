fdc9f7fabab770cf31a0351108eac18e1a5a4289
Xavier Bustamante Talavera <xavier@bustawin.com>
2018-05-08 17:17:00 +0200

    Store SMART values

--- ereuse_devicehub/resources/event/device/test_hard_drive/settings.py
+++ ereuse_devicehub/resources/event/device/test_hard_drive/settings.py
@@ -1,8 +1,12 @@
-from ereuse_devicehub.resources.event.device.settings import parent_materialized, EventWithOneDevice, \
-    EventSubSettingsOneDevice
+from ereuse_devicehub.resources.event.device.settings import EventSubSettingsOneDevice, \
+    EventWithOneDevice, parent_materialized
 
 
 class TestHardDrive(EventWithOneDevice):
+    """
+    We decided to take these specific SMART values because of
+    https://www.backblaze.com/blog/hard-drive-smart-stats/.
+    """
     type = {
         'type': 'string',
         # 'allowed': ['Short Offline', 'Extended Offline']
@@ -34,6 +38,27 @@ class TestHardDrive(EventWithOneDevice):
         'type': 'integer'
     }
     parent = parent_materialized
+    reallocatedSectorCount = {
+        'type': 'integer'
+    }
+    powerCycleCount = {
+        'type': 'integer'
+    }
+    reportedUncorrectableErrors = {
+        'type': 'integer'
+    }
+    CommandTimeout = {
+        'type': 'integer'
+    }
+    CurrentPendingSectorCount = {
+        'type': 'integer'
+    }
+    OfflineUncorrectable = {
+        'type': 'integer'
+    }
+    RemainingLifetimePercentage = {
+        'type': 'integer'
+    }
 
 
 class TestHardDriveSettings(EventSubSettingsOneDevice):
